d9599d193406a83d079a4d70ce74929bc3318e6e
Mourad <mourradmohsen@gmail.com>
2019-08-06 20:46:30 +0200

    changes

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+agent any
+ stages {
+  stage("SCM") {
+   steps {
+    checkout SCM
+    }
+   }
+  stage("Build")
+   parallel {
+    stage("Compile") {
+     agent {
+      docker {
+       image 'maven:3.6.0-jdk-8-alpine'
+       args '-v /root/.m2/repository:/root/.m2/repository'
+       // to use the same node and workdir defined on top-level pipeline for all docker agents
+       reuseNode true
+      }
+     }
+     steps {
+      sh "mvn clean compile"
+     }
+    }
+   }
+  }
+}
